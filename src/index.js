91493023824655e7d8308c68c8a1182b02ad8e50
arohen24 <arohen24@gmail.com>
2025-05-26 00:58:58 +0900

    fix: discard unnecessary module

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client'; // 또는 'react-dom' (Create React App 버전에 따라 다름)
 import './index.css'; // 기본 CSS 파일이 있다면
 import App from './App'; // App 컴포넌트를 불러옵니다.
-import reportWebVitals from './reportWebVitals'; // 성능 측정 파일, 없다면 이 줄과 아래 호출은 삭제 가능
 
 // React 18 이상 버전 (권장)
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -19,7 +18,3 @@ root.render(
 //   </React.StrictMode>,
 //   document.getElementById('root')
 // );
-
-
-// 앱 성능 측정 (선택 사항)
-reportWebVitals();
